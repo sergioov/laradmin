7616dfa8c2c76cb89dd61135060415f360e37549
Sergio Orozco <sergioov@hotmail.com>
2016-07-23 12:24:51 -0500

    Add one regitered user lar.queretaro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,22 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+
+#These settings are for the sending out email for active admin and consequently the   devise mailer
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+#ActionMailer::Base.smtp_settings = 
+#{
+#
+#  :address            => 'smtp.gmail.com',
+#  :port               => 587,
+#  :domain             => 'gmail.com', #you can also use google.com
+#  :authentication     => :plain,
+#  :user_name          => 'laradmin@gmail.com',
+#  :password           => 'XXXXXXX'
+#}
   
 end
